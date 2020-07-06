98b77bfd55e6ce9b6c95b1d63613483fcf289f8a
Arkadiy <arkadi97@mail.ru>
2020-07-06 15:29:24 +0300

    upd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-     System.out.println("it's test");
+     System.out.println("it's test 2");
+     System.out.println("it's test 3");
     }
 }
